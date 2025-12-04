281a951d23d35b7472ef4b2119f8e4dba643f0fa
AnnaDueholm <eaa25alds@students.eaaa.dk>
2025-12-04 15:00:07 +0100

    Funktionel produktside
    Jeg har lavet min produktside med billeder, produktinfo og gjort den interaktiv vha. JS

--- Eksamen-LuminaOne/js.js
+++ Eksamen-LuminaOne/js.js
@@ -1,1 +1,25 @@
+//opretter forbindelse til mit body element i html
+const body = document.body;
 
+//henter mine elementer/billeder med klassen "color" og definerer dem som en liste/"slides"
+const slides = document.querySelectorAll(".color");
+
+//henter mine knapper (farve ellipser)
+const whiteBtn = document.getElementById("white");
+const blackBtn = document.getElementById("black");
+const silverBtn = document.getElementById("silver");
+const brownBtn = document.getElementById("brown");
+
+//opretter en variabel med navnet "activeSlide", og fortæller at der er 4 aktive slides i den (0 til 3)
+
+let activeSlide = 3;
+
+function setActiveSlide(index) {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+}
+
+whiteBtn.addEventListener("click", () => setActiveSlide(0));
+blackBtn.addEventListener("click", () => setActiveSlide(1));
+silverBtn.addEventListener("click", () => setActiveSlide(2));
+brownBtn.addEventListener("click", () => setActiveSlide(3));
